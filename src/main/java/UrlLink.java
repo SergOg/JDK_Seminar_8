eb37b3875962d88e1671c13af5c437561f471de7
SergOg <82341979+SergOg@users.noreply.github.com>
2024-09-26 19:50:40 +0300

    Update UrlLink.java

--- src/main/java/UrlLink.java
+++ src/main/java/UrlLink.java
@@ -1,15 +1,22 @@
-public class UrlLink {
+import java.util.Random;
+
+public class UrlLink extends Thread{
     private String name;
 
     public UrlLink(String name) {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public void run() {
+        try {
+            System.out.println("\n" + Thread.currentThread().getName());
+            DownloadFile.loader(name);
+            Thread.sleep(new Random().nextInt(999));
+            System.out.println("Загрузка файла: " + name + " завершена!");
+            Thread.interrupted();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
